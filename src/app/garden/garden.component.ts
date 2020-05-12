ee10cb897030f1296df02f42f8c9820e7a25f7e4
Venus Yu <venus0yu@gmail.com>
2020-05-12 18:35:39 -0400

    beginning

--- src/app/garden/garden.component.ts
+++ src/app/garden/garden.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { flowers, colors } from '../../enums';
 
 @Component({
   selector: 'app-garden',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GardenComponent implements OnInit {
 
+  gridSize = 20;
+  grid = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.grid = new Array(this.gridSize).fill(0).map(() => new Array(this.gridSize).fill(0));
+
+    console.log(this.grid);
   }
 
 }
---
+++ src/enums.ts
@@ -0,0 +1,25 @@
+const flowers = [
+    'cosmos',
+    'hyacinth',
+    'lily',
+    'mum',
+    'pansy',
+    'rose',
+    'tulip',
+    'windflower'
+];
+
+const colors = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'purple',
+    'pink',
+    'white',
+    'black',
+    'gold'
+]
+
+export { flowers, colors }
